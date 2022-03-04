20e8c185190d8568488d6960d1ceb376260cef1a
olesien <olesien2@gmail.com>
2022-03-04 12:47:23 +0100

    added connection cleardb URL

--- models/index.js
+++ models/index.js
@@ -1,24 +1,24 @@
 // Setting up the database connection
-const knex = require('knex')({
-	debug: true,
-	client: 'mysql',
-	connection: {
-		host: process.env.DB_HOST || 'localhost',
-		port: process.env.DB_PORT || 3306,
-		user: process.env.DB_USER || 'library',
-		password: process.env.DB_PASSWORD || '',
-		database: process.env.DB_NAME || 'library',
-	}
+const knex = require("knex")({
+    debug: true,
+    client: "mysql",
+    connection: process.env.CLEARDB_DATABASE_URL || {
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 3306,
+        user: process.env.DB_USER || "library",
+        password: process.env.DB_PASSWORD || "",
+        database: process.env.DB_NAME || "library",
+    },
 });
 
-const bookshelf = require('bookshelf')(knex);
+const bookshelf = require("bookshelf")(knex);
 
 const models = {};
-models.Author = require('./Author')(bookshelf);
-models.Book = require('./Book')(bookshelf);
-models.User = require('./User')(bookshelf);
+models.Author = require("./Author")(bookshelf);
+models.Book = require("./Book")(bookshelf);
+models.User = require("./User")(bookshelf);
 
 module.exports = {
-	bookshelf,
-	...models,
+    bookshelf,
+    ...models,
 };
